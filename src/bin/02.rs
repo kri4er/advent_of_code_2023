ffc5a6891694ae9f2422771a73a86f23607113ae
kri4er <mynamekaldery@gmail.com>
2023-12-23 19:13:35 +0100

    day2 - part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,5 +1,6 @@
 advent_of_code::solution!(2);
 use std::{str::FromStr, collections::HashMap};
+use std::cmp;
 
 #[derive(Debug, PartialEq)]
 pub struct Game {
@@ -38,16 +39,32 @@ impl FromStr for Game {
 pub fn is_game_possible(game: &Game, conditions: &HashMap<&str, u32>) -> bool {
     for cube_set in &game.cubes {
         for cube in cube_set {
-            let color:&str = cube[1].as_ref();
+            //let color:&str = cube[1].as_ref();
+            let color = cube[1].as_ref();
             let value:u32 = cube[0].parse::<u32>().unwrap();
-
             //println!("color: {:?}, value: {:?}", &color, &value);
-            if conditions.get(color).unwrap().to_owned() < value {
+            if conditions.get(&color).unwrap().to_owned() < value {
                 return false
             }
         }
     }
     true
+}
+
+pub fn evaluate_cubes_to_make_game_possible(game: &Game) -> u32 {
+    let mut m = HashMap::<&str, u32>::new();
+
+    for cube_set in &game.cubes {
+        for cube in cube_set {
+            let color:&str = cube[1].as_ref();
+            let value:u32 = cube[0].parse::<u32>().unwrap();
+            m.insert(color, if m.contains_key(color) { cmp::max(m[color], value) } else { value });
+        }
+    }
+    return m.into_iter().fold(1, |mut acc, value:(&str, u32)|{
+        acc *= value.1.to_owned();
+        acc
+    })
 }
 
 
@@ -68,7 +85,13 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let result = input.split("\n")
+        .filter(|r| !r.is_empty())
+        .map(|r| Game::from_str(r).unwrap())
+        .map(|game| evaluate_cubes_to_make_game_possible(&game))
+        .sum::<u32>();
+
+    return Some(result)
 }
 
 #[cfg(test)]
@@ -79,12 +102,20 @@ mod test_day2 {
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
 
-        assert_eq!(result, Some(1));
+        assert_eq!(result, Some(8));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2286));
+    }
+
+    #[test]
+    fn hash_map_insertion() {
+        let mut my_map = HashMap::<&str, u32>::new();
+        my_map.insert("red", if my_map.contains_key("red") { cmp::max(my_map["red"], 11) } else { 11 });
+
+        assert_eq!(my_map["red"], 11);
     }
 }
