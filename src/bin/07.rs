da9ee499569f6b59cecdade7a51a3a966ff0ef65
kri4er <mynamekaldery@gmail.com>
2024-01-01 11:12:14 +0100

    day7 - part 2

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -48,11 +48,55 @@ fn eval_power(set: &str) -> i64 {
     for (_, val) in numbers.into_iter().enumerate() {
         m.insert(val, if m.contains_key(&val) { m[&val] + 1 } else { 1 });
     }
-    let mut counter:Vec<u32> = m.into_iter().map(|(_, val)| val).collect();
+    let mut counter:Vec<u32> = m.clone().into_iter()
+        .map(|(_, val)| val).collect();
 
     counter.sort();
 
-    match counter.as_slice() {
+    return match counter.as_slice() {
+        [5] => 10,
+        [1, 4] => 9,
+        [2, 3] => 8,
+        [1, 1, 3] => 7,
+        [1, 2, 2] => 6,
+        [1, 1, 1, 2] => 5,
+        [1, 1, 1, 1, 1] => 4,
+        [] => panic!("Can't match card, error"),
+        _ => panic!("Can't match card, error"),
+    }
+}
+
+fn eval_power_2(set: &str) -> i64 {
+    let numbers:Vec<u32> = set.chars()
+        .map(|c|
+             match c {
+                 'A' => 14,
+                 'K' => 13,
+                 'Q' => 12,
+                 'J' => 1,
+                 'T' => 10,
+                 n => n.to_digit(10).unwrap(),
+             }).collect();
+
+    let mut m:HashMap<u32, u32> = HashMap::new();
+    for (_, val) in numbers.into_iter().enumerate() {
+        m.insert(val, if m.contains_key(&val) { m[&val] + 1 } else { 1 });
+    }
+    let mut counter:Vec<u32> = m.clone().into_iter()
+        //We filter out jobkers from evaluation
+        .filter(|(k, _)| k != &(1 as u32))
+        .map(|(_, val)| val).collect();
+
+    counter.sort();
+    if let Some(last) = counter.last_mut() {
+        //now, when we sorted the array, we can use existing jokers to add power to our hand
+        *last += if m.contains_key(&(1 as u32)) { m[&(1 as u32)] } else { 0 };
+    } else {
+        //Only jokers are in the list:
+        return 10;
+    }
+
+    return match counter.as_slice() {
         [5] => 10,
         [1, 4] => 9,
         [2, 3] => 8,
@@ -101,8 +145,42 @@ pub fn part_one(input: &str) -> Option<i64> {
     Some(result)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<i64> {
+    let mut cards:Vec<Card> = input
+        .split("\n")
+        .filter(|row| !row.is_empty())
+        .map(|line| {
+            let (card_set, bid_str) = line.split_once(" ").unwrap();
+            let bid:i64 = bid_str.parse().unwrap();
+            (card_set, bid)
+        })
+        .map(|card|
+             Card{
+                 //Since A is lower than T in lexicographical order, we should remap Named cards
+                 //with ascending ordered chars, later used to sort matching card sets
+                 set: card.0.clone().chars().map(|c|
+                                                 match c {
+                                                     'A' => char::from_u32(58+5).unwrap(),
+                                                     'K' => char::from_u32(58+4).unwrap(),
+                                                     'Q' => char::from_u32(58+3).unwrap(),
+                                                     //J is weaker then any other card so we map it
+                                                     //to 49 == char :'1'
+                                                     'J' => char::from_u32(49).unwrap(),
+                                                     'T' => char::from_u32(58+1).unwrap(),
+                                                     n => n,
+                                                 }).collect(),
+                 set_strenght: eval_power_2(&card.0),
+                 bid: card.1,
+             })
+        .collect();
+
+    cards.sort();
+    let mut result:i64 = 0;
+    for (i, _) in cards.iter().rev().enumerate() {
+        result += (i as i64 + 1) * &cards[i].bid;
+    }
+
+    Some(result)
 }
 
 #[cfg(test)]
@@ -123,65 +201,50 @@ mod test_day7 {
             let c1 = "K";
             let c2 = "T";
             let comp = c1.cmp(&c2);
-            println!("COMP IS LOGME: {:?}", comp);
             assert_eq!(Ordering::Less, comp);
         }
         {
             let c1 = "Q";
             let c2 = "T";
             let comp = c1.cmp(&c2);
-            println!("COMP IS LOGME: {:?}", comp);
             assert_eq!(Ordering::Less, comp);
         }
         {
             let c1 = "A";
             let c2 = "T";
             let comp = c1.cmp(&c2);
-            println!("COMP IS LOGME: {:?}", comp);
             assert_eq!(Ordering::Less, comp);
         }
         {
             let c1 = "J";
             let c2 = "T";
             let comp = c1.cmp(&c2);
-            println!("COMP IS LOGME: {:?}", comp);
             assert_eq!(Ordering::Less, comp);
         }
         {
-            println!("LOGME Start of test");
             let c1 = "KQK77";
             let c2 = "KTJJT";
             let left = Card{set: c1.into(), set_strenght: eval_power(&c1), bid: 28 };
             let right = Card{set: c2.into(), set_strenght: eval_power(&c2), bid: 220 };
             let mut ve:Vec<Card> = vec![left.into(), right.into()];
-            println!("LOGME before: {:?}", ve);
             ve.sort();
-            println!("LOGME after sort: {:?}", ve);
             let mut result:i64 = 0;
             for (i, _) in ve.iter().rev().enumerate() {
-                println!("Card: {:?} == {:?}", i + 1, ve[i]);
                 result += (i as i64 + 1) * &ve[i].bid;
             }
-            println!("LOGME End of test");
-            //let ord = left.cmp(&right);
             //assert_eq!(ord, Ordering::Greater);
         }
         {
-            println!("LOGME Start of test");
             let c1 = "22272";
             let c2 = "22223";
             let left = Card{set: c1.into(), set_strenght: eval_power(&c1), bid: 28 };
             let right = Card{set: c2.into(), set_strenght: eval_power(&c2), bid: 220 };
             let mut ve:Vec<Card> = vec![left.into(), right.into()];
-            println!("LOGME before: {:?}", ve);
             ve.sort();
-            println!("LOGME after sort: {:?}", ve);
             let mut result:i64 = 0;
             for (i, _) in ve.iter().rev().enumerate() {
-                println!("Card: {:?} == {:?}", i + 1, ve[i]);
                 result += (i as i64 + 1) * &ve[i].bid;
             }
-            println!("LOGME End of test");
             //let ord = left.cmp(&right);
             //assert_eq!(ord, Ordering::Greater);
         }
@@ -213,6 +276,6 @@ mod test_day7 {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(5905));
     }
 }
