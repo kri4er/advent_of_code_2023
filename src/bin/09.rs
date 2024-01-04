d86fbf1205c393af75b63e648871823c945d3d5b
kri4er <mynamekaldery@gmail.com>
2024-01-05 00:20:05 +0100

    day 9 - part 2

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -36,8 +36,22 @@ pub fn part_one(input: &str) -> Option<i64> {
     Some(result)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+fn compute_prev_num(nums: &[i64]) -> i64 {
+    if nums.iter().all(|&n| n == 0) {
+        return 0;
+    }
+    let differences = eval_diff(nums);
+    nums[0] - compute_prev_num(&differences)
+}
+
+pub fn part_two(input: &str) -> Option<i64> {
+    let result = input
+        .lines()
+        .map(|line| str_to_vec(line))
+        .map(|data| compute_prev_num(&data))
+        .sum();
+
+    Some(result)
 }
 
 #[cfg(test)]
@@ -53,6 +67,6 @@ mod test_day9 {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2));
     }
 }
